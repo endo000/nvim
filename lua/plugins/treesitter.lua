39a1c09c8b45a2fa0a88e0a77fa90c088e20c6a2
Aleksandr Shishkov <aleksandr.shishkov@result.si>
2023-04-04 11:40:24 +0200

    Add additional treesitter parsers

--- lua/plugins/treesitter.lua
+++ lua/plugins/treesitter.lua
@@ -1,6 +1,9 @@
 return {
   {
     "nvim-treesitter/nvim-treesitter",
+    opts = function(_, opts)
+      vim.list_extend(opts.ensure_installed, { "c_sharp", "go" })
+    end,
     config = function(_, opts)
       require("nvim-treesitter.configs").setup(opts)
 
